5d3da155401ed070e61f68221c971a94d6adaeed
musupadi <33887565+musupadi@users.noreply.github.com>
2020-07-10 17:54:08 +0700

    Tambah Activity

--- app/src/main/java/com/destinyapp/absensi/Activity/SplashActivity.java
+++ app/src/main/java/com/destinyapp/absensi/Activity/SplashActivity.java
@@ -2,9 +2,12 @@ package com.destinyapp.absensi.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.animation.Animator;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -15,6 +18,8 @@ public class SplashActivity extends AppCompatActivity {
     LottieAnimationView anim;
     DB_Helper dbHelper;
     String username,nama,divisi,level;
+    private static final int PERMISSION_STORAGE_CODE = 1000;
+    int READ_PHONE_STATE_PERMISSION = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +35,7 @@ public class SplashActivity extends AppCompatActivity {
             }
         }
         if (username != null){
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            ChangeActivity();
         }
         anim = findViewById(R.id.animation);
         anim.addAnimatorListener(new Animator.AnimatorListener() {
@@ -43,9 +46,28 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animator animator) {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED){
+                        //
+                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permissions,PERMISSION_STORAGE_CODE);
+                        if(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) ==
+                                PackageManager.PERMISSION_DENIED){
+                            //
+                            String[] permissionss = {Manifest.permission.READ_PHONE_STATE};
+                            requestPermissions(permissionss,READ_PHONE_STATE_PERMISSION);
+                        }
+                    }else{
+                        ChangeActivity();
+                    }
+                }else{
+                    ChangeActivity();
+                }
+                ChangeActivity();
+
+
+
             }
 
             @Override
@@ -58,5 +80,10 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void ChangeActivity(){
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
